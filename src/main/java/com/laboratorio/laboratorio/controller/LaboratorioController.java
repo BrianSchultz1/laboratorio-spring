35e1316cd164b9391007a6b296b469210bbe7be8
Lucas Silva <lucasdoismile16@gmail.com>
2023-11-30 23:28:15 -0300

    refact: service and controller

--- src/main/java/com/laboratorio/laboratorio/controller/LaboratorioController.java
+++ src/main/java/com/laboratorio/laboratorio/controller/LaboratorioController.java
@@ -1,11 +1,20 @@
 package com.laboratorio.laboratorio.controller;
 
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.laboratorio.laboratorio.Laboratorio;
 import com.laboratorio.laboratorio.service.LaboratorioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/laboratorios")
@@ -23,10 +32,39 @@ public class LaboratorioController {
         return laboratorioService.getAllLaboratorios();
     }
 
-    @GetMapping("/disponiveis")
-    public List<Laboratorio> getLaboratoriosDisponiveis() {
-        return laboratorioService.getLaboratoriosDisponiveis();
+    @GetMapping("/{id}")
+    public ResponseEntity<Laboratorio> getLaboratorioById(@PathVariable Long id) {
+        Laboratorio laboratorio = laboratorioService.getLaboratorioById(id);
+        if (laboratorio != null) {
+            return new ResponseEntity<>(laboratorio, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    // Implemente CRUD completo
+    @PostMapping
+    public ResponseEntity<Laboratorio> createLaboratorio(@RequestBody Laboratorio laboratorio) {
+        Laboratorio createdLaboratorio = laboratorioService.createLaboratorio(laboratorio);
+        return new ResponseEntity<>(createdLaboratorio, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Laboratorio> updateLaboratorio(@PathVariable Long id, @RequestBody Laboratorio laboratorio) {
+        Laboratorio updatedLaboratorio = laboratorioService.updateLaboratorio(id, laboratorio);
+        if (updatedLaboratorio != null) {
+            return new ResponseEntity<>(updatedLaboratorio, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLaboratorio(@PathVariable Long id) {
+        boolean deleted = laboratorioService.deleteLaboratorio(id);
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/laboratorio/laboratorio/service/LaboratorioService.java
+++ src/main/java/com/laboratorio/laboratorio/service/LaboratorioService.java
@@ -1,19 +1,52 @@
 package com.laboratorio.laboratorio.service;
 
-import com.laboratorio.laboratorio.Laboratorio;
-
 import java.util.List;
 
-public interface LaboratorioService {
-    List<Laboratorio> getAllLaboratorios();
+import org.jvnet.hk2.annotations.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
-    List<Laboratorio> getLaboratoriosDisponiveis();
+import com.laboratorio.laboratorio.Laboratorio;
+import com.laboratorio.laboratorio.repository.LaboratorioRepository;
 
-    Laboratorio getLaboratorioById(Long id);
+@Service
+public class LaboratorioService {
 
-    Laboratorio saveLaboratorio(Laboratorio laboratorio);
+    @Autowired
+    private LaboratorioRepository laboratorioRepository;
 
-    Laboratorio updateLaboratorio(Long id, Laboratorio laboratorio);
+    public List<Laboratorio> getAllLaboratorios() {
+        return laboratorioRepository.findAll();
+    }
 
-    void deleteLaboratorio(Long id);
+    public Laboratorio getLaboratorioById(Long id) {
+        return laboratorioRepository.findById(id).orElse(null);
+    }
+
+    public Laboratorio createLaboratorio(Laboratorio laboratorio) {
+        return laboratorioRepository.save(laboratorio);
+    }
+
+    public Laboratorio updateLaboratorio(Long id, Laboratorio updatedLaboratorio) {
+        Laboratorio existingLaboratorio = laboratorioRepository.findById(id).orElse(null);
+        if (existingLaboratorio != null) {
+            // Atualize os campos relevantes do laboratório existente com base no updatedLaboratorio
+            existingLaboratorio.setNome(updatedLaboratorio.getNome());
+            existingLaboratorio.setDescricao(updatedLaboratorio.getDescricao());
+            // Adicione mais campos conforme necessário
+
+            return laboratorioRepository.save(existingLaboratorio);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteLaboratorio(Long id) {
+        Laboratorio existingLaboratorio = laboratorioRepository.findById(id).orElse(null);
+        if (existingLaboratorio != null) {
+            laboratorioRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
